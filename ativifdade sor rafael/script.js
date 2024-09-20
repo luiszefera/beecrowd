e0c93f8dae8a86de7f0f28ea87c61b1aeb5bf7e0
luiszefera <luis18zeferino@outlook.com>
2024-09-19 22:09:09 -0300

    Add files via upload

---
+++ ativifdade sor rafael/script.js
@@ -0,0 +1,69 @@
+function exercicio1010(){
+    let codigo,numero,valor
+    let Number = (prompt('digite o codigo'))
+
+    
+
+}
+
+function simelarcorrida(){
+    let distsancia = Number(prompt("quala distancia desejada"))
+    let distanciax =0;
+    let distanciay =0;
+    tempo =0
+    while(distanciax-distanciay <distsancia){
+        distanciax +=1
+        distanciay +=1.5
+        tempo++
+        console.log('y:'+ distanciay);
+    console.log('x:'+distanciax)
+    console.log('tempo:'+tempo)
+    console.log('distancia:' + (distanciay-distanciax));
+    console.log("      ")
+    alert( 'informe distncia')
+
+
+
+
+
+    
+    }  
+}
+function exercicio1038(){
+     let codigo = Number(prompt("digite o codigo:"))
+     let quantidade = Number(prompt("digite quantidade"))
+     let preco
+     if(codigo ==1){
+     preco== 4}
+else if(codigo==2){
+preco= 4.5}
+        else if(codigo==3){
+            preco =5}
+        else if(codigo==4){
+        preco= 2
+        }
+        else if (codigo==5){
+            preco= 1.5
+
+        }
+         alert('quantidade de item ')
+
+
+         
+
+        }
+        function ( exercicio1008){
+         let n1, horas, salario 
+            
+         let= Number (prompt("digite as horas trabalhadas"))
+         let= Number( prompt("duigete o valor de seu salario"))
+         alert ("horas trabalhdas= salario ") 
+          
+
+
+        }
+     
+
+
+
+
